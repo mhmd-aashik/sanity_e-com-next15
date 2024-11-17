be4da2f7e9ca77c37330a367dadac1144bffffc9
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-18 01:15:14 +0530

    Building the Product Thumbnail Component

--- components/home/ProductGrid.tsx
+++ components/home/ProductGrid.tsx
@@ -2,6 +2,7 @@
 
 import { Product } from "@/sanity.types";
 import { AnimatePresence, motion } from "framer-motion";
+import ProductThumb from "./ProductThumb";
 
 function ProductGrid({ products }: { products: Product[] }) {
   return (
@@ -16,7 +17,7 @@ function ProductGrid({ products }: { products: Product[] }) {
               exit={{ opacity: 0 }}
               className="flex justify-center"
             >
-              {/* <ProductThumb key={product._id} product={product} /> */}
+              <ProductThumb key={product._id} product={product} />
             </motion.div>
           </AnimatePresence>
         );
