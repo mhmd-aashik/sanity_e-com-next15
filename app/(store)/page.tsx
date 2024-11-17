cb45cfdf75bd834009e633dfcad3d3b4f2a6af31
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-18 00:20:10 +0530

    Building a Helper Function to Get All Products with GROQ Query

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
   return (
     <div>
       <h1>Hello World 123</h1>
