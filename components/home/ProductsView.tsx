eee36a5a8227e04c18f1abe66e70f75563f93ce5
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-18 02:57:16 +0530

    Building the Category Page

--- components/home/ProductsView.tsx
+++ components/home/ProductsView.tsx
@@ -1,5 +1,6 @@
 import { Category, Product } from "@/sanity.types";
 import ProductGrid from "./ProductGrid";
+import { CategorySelectorComponent } from "../ui/CategorySelectorComponent";
 
 interface ProductsViewProps {
   products: Product[];
@@ -9,7 +10,9 @@ interface ProductsViewProps {
 const ProductsView = ({ products, categories }: ProductsViewProps) => {
   return (
     <div className="flex flex-col">
-      <div className="w-full sm:w-[200px]"></div>
+      <div className="w-full sm:w-[200px]">
+        <CategorySelectorComponent categories={categories} />
+      </div>
       <div className="flex-1">
         <div>
           <ProductGrid products={products} />
