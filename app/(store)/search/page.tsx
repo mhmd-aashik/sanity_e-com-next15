4214e62e9ea1f377fb339d0da32a96389df54c52
mhmd-aashik <aashikdevelop@gmail.com>
2024-11-18 02:04:32 +0530

    Building the Search Page

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,3 +1,5 @@
+import ProductGrid from "@/components/home/ProductGrid";
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 import React from "react";
 
 const SearchPage = async ({
@@ -8,8 +10,32 @@ const SearchPage = async ({
   };
 }) => {
   const { query } = await searchParams;
-  console.log(query);
-  return <div>SearchPage {query}</div>;
+  const products = await searchProductsByName(query);
+
+  if (!products.length) {
+    return (
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+          <h1 className="text-3xl font-bold mb-6 text-center">
+            No products found for: {query}
+          </h1>
+          <p className="text-gray-600 text-center">
+            Try searching with different keywords
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+          Search results for {query}
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
--- sanity.types.ts
+++ sanity.types.ts
@@ -280,7 +280,7 @@ export type SanityImageMetadata = {
 
 export type AllSanitySchemaTypes = SanityImagePaletteSwatch | SanityImagePalette | SanityImageDimensions | SanityFileAsset | Geopoint | Sale | Order | Product | Category | Slug | BlockContent | SanityImageCrop | SanityImageHotspot | SanityImageAsset | SanityAssetSourceData | SanityImageMetadata;
 export declare const internalGroqTypeReferenceTo: unique symbol;
-// Source: ./sanity/lib/products/getAllCategories.tsx
+// Source: ./sanity/lib/products/getAllCategories.ts
 // Variable: ALL_CATEGORIES_QUERY
 // Query: *[_type == "category"] | order(name asc)
 export type ALL_CATEGORIES_QUERYResult = Array<{
@@ -298,6 +298,69 @@ export type ALL_CATEGORIES_QUERYResult = Array<{
 // Variable: ALL_PRODUCTS_QUERY
 // Query: *[                _type == "product"            ] | order(name asc)
 export type ALL_PRODUCTS_QUERYResult = Array<{
+  _id: string;
+  _type: "product";
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  name?: string;
+  slug?: Slug;
+  image?: {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    _type: "image";
+  };
+  description?: Array<{
+    children?: Array<{
+      marks?: Array<string>;
+      text?: string;
+      _type: "span";
+      _key: string;
+    }>;
+    style?: "blockquote" | "h1" | "h2" | "h3" | "h4" | "normal";
+    listItem?: "bullet";
+    markDefs?: Array<{
+      href?: string;
+      _type: "link";
+      _key: string;
+    }>;
+    level?: number;
+    _type: "block";
+    _key: string;
+  } | {
+    asset?: {
+      _ref: string;
+      _type: "reference";
+      _weak?: boolean;
+      [internalGroqTypeReferenceTo]?: "sanity.imageAsset";
+    };
+    hotspot?: SanityImageHotspot;
+    crop?: SanityImageCrop;
+    alt?: string;
+    _type: "image";
+    _key: string;
+  }>;
+  price?: number;
+  categories?: Array<{
+    _ref: string;
+    _type: "reference";
+    _weak?: boolean;
+    _key: string;
+    [internalGroqTypeReferenceTo]?: "category";
+  }>;
+  stock?: number;
+}>;
+
+// Source: ./sanity/lib/products/searchProductsByName.ts
+// Variable: PRODUCTS_BY_CATEGORY_QUERY
+// Query: *[          _type == "product"           && references(*[_type == "category" && slug.current == $categorySlug]._id)        ] | order(name asc)
+export type PRODUCTS_BY_CATEGORY_QUERYResult = Array<{
   _id: string;
   _type: "product";
   _createdAt: string;
@@ -381,6 +444,7 @@ declare module "@sanity/client" {
   interface SanityQueries {
     "\n        *[_type == \"category\"] | order(name asc)\n      ": ALL_CATEGORIES_QUERYResult;
     "\n            *[\n                _type == \"product\"\n            ] | order(name asc) \n        ": ALL_PRODUCTS_QUERYResult;
+    "\n        *[\n          _type == \"product\" \n          && references(*[_type == \"category\" && slug.current == $categorySlug]._id)\n        ] | order(name asc)\n      ": PRODUCTS_BY_CATEGORY_QUERYResult;
     "\n        *[\n          _type == \"sale\" \n          && isActive == true \n          && couponCode == $couponCode\n        ] | order(validFrom desc)[0]\n      ": ACTIVE_SALE_BY_COUPON_QUERYResult;
   }
 }
